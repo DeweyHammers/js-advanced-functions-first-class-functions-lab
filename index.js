4f21dc0fa5b994ae3cfd89a1a0989008a65b5383
Dewey <hammersdrichard@gmail.com>
2021-06-25 11:12:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+const returnFirstTwoDrivers = (array) => {
+  return array.slice(0, 2);
+} 
+
 const returnLastTwoDrivers = (array) => {
-  return array
+  return array.slice(2, array.length);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (num) => {
+  return () => {return num * num}
+}
+
+const fareDoubler = (num) => {
+  return num += num
+}
+
+const fareTripler = (num) => {
+  return num + num + num
+}
+
+const selectDifferentDrivers = (drivers, func) => {
+  return func(drivers)
 }
